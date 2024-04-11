c4a80e37745670bdd92cb5c66d6afc039c3be540
ClaraJacot <clara.jacot@gmail.com>
2024-04-11 09:58:02 +0200

    ajout de creat posts au dashboard

--- resources/views/posts/postsCreate.blade.php
+++ resources/views/posts/postsCreate.blade.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-    </head>
-    <body>
+<section>
+    
       <h1>Make a post</h1>
       
       <form method="POST" action="{{ route('postsStore') }}" enctype="multipart/form-data" >
@@ -43,6 +31,7 @@
 			<div>{{ $message }}</div>
 			@enderror
 		</p>
+        
         <input type="submit" name="valider" value="Valider" >
       </form>
-    </body>
+</section>
