9bcc17180b4401f2122f3fc65f228a18fd73e47e
Néïs ZONCA-CROS <146724473+neis-zonca-cros@users.noreply.github.com>
2024-04-16 14:10:04 +0200

    textarea

--- resources/views/posts/postsCreate.blade.php
+++ resources/views/posts/postsCreate.blade.php
@@ -9,34 +9,25 @@
 			<label for="titre" >Titre</label><br/>
 			<textarea class="text-black" type="text" name="titre" value="{{ old('titre') }}"  id="titre" placeholder="Le titre du post" ></textarea>
 
-			<!-- Le message d'erreur pour "title" -->
-			@error("titre")
-			<div>{{ $message }}</div>
-			@enderror
+
 		</p>
 	   </div>
 		<div class="flex justify-center text-center p-6">
         <p class="m-2 ">
 			<label for="texte"  >Contenu</label><br/>
-			<textarea class="h-48 text-black" name="texte" id="texte" placeholder="Le contenu du post">{{ old('texte') }}</textarea>
-			<!-- Le message d'erreur pour "texte" -->
-			@error("texte")
-			<div>{{ $message }}</div>
-			@enderror
+			<textarea class="h-48 text-black" type="text" name="texte" value="{{ old('texte') }}"  id="texte" placeholder="Le contenu du post"></textarea>
+
 		</p>
 	</div>
         <p>
 			
 			<input type="hidden" name="user_id" value="{{Auth::user()->id}}"  id="user_id">
 
-			<!-- Le message d'erreur pour "title" -->
-			@error("user_id")
-			<div>{{ $message }}</div>
-			@enderror
+			
 		</p>
 
         <div class="p-2 flex justify-center">
-        <x-primary-button>{{ __('Sauvegarder') }}</x-primary-button>
+			<x-primary-button>{{ __('Sauvegarder') }}</x-primary-button>		
 		</div>
       </form>
 </section>
