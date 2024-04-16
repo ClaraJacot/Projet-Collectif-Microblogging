564bad00463bb9e2596122740465a900b2658229
Néïs ZONCA-CROS <146724473+neis-zonca-cros@users.noreply.github.com>
2024-04-16 11:15:06 +0200

    profil

--- resources/views/myProfile.blade.php
+++ resources/views/myProfile.blade.php
@@ -4,16 +4,17 @@
             {{ __('Profile') }}
         </h2>
     </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="grid grid-cols-none md:grid-cols-3 lg:grid-cols-3 justify-center">
+<div class="flex flex-col col-span-1 justify-center max-w-screen-sm mx-auto sm:px-6 lg:px-8"> 
+    <div class="py-12 flex justify-center">
+        <div class="w-96 mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     @include('users.usersShow')
-                    <div class="flex items-center gap-4">
+                    <div class="pt-6 flex items-center justify-center gap-4">
                         @if (Auth::user()->id === $user->id)
                         <a href="{{ route('profile.edit') }}">
-                            <x-primary-button>{{ __('Edit') }}</x-primary-button>
+                            <x-primary-button>{{ __('Modifier') }}</x-primary-button>
                         </a>
                         @endif
                     </div>
@@ -21,9 +22,10 @@
             </div>
         </div>
     </div>
+
     @if (Auth::user()->id === $user->id)
-<div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+<div class="py-12 flex justify-center">
+    <div class="w-96 mx-auto sm:px-6 lg:px-8">
         <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 text-gray-900 dark:text-gray-100">
                 @include('posts.postsCreate')
@@ -32,8 +34,10 @@
     </div>
 </div>
 @endif
-<div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+</div>
+
+<div class="py-12 col-span-2">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 ">
         <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
             <div class="p-6 text-gray-900 dark:text-gray-100">
                 @include('posts.postIndex')
@@ -41,4 +45,6 @@
         </div>
     </div>
 </div>
+</div>
+
 </x-app-layout>
--- resources/views/posts/postsCreate.blade.php
+++ resources/views/posts/postsCreate.blade.php
@@ -1,27 +1,30 @@
 <section>
     
-      <h1>Make a post</h1>
+      <h1 class="text-center uppercase">Créer un post</h1>
       
       <form method="POST" action="{{ route('postsStore') }}" enctype="multipart/form-data" >
        @csrf 
+	   <div class="flex justify-center text-center p-6">
         <p>
 			<label for="titre" >Titre</label><br/>
-			<input type="text" name="titre" value="{{ old('titre') }}"  id="titre" placeholder="Le titre du post" >
+			<textarea class="text-black" type="text" name="titre" value="{{ old('titre') }}"  id="titre" placeholder="Le titre du post" ></textarea>
 
 			<!-- Le message d'erreur pour "title" -->
 			@error("titre")
 			<div>{{ $message }}</div>
 			@enderror
 		</p>
-        <p>
-			<label for="texte" >Contenu</label><br/>
-			<input type="text" name="texte" value="{{ old('texte') }}"  id="texte" placeholder="Le contenu du post" >
-
+	   </div>
+		<div class="flex justify-center text-center p-6">
+        <p class="m-2 ">
+			<label for="texte"  >Contenu</label><br/>
+			<textarea class="h-48 text-black" name="texte" id="texte" placeholder="Le contenu du post">{{ old('texte') }}</textarea>
 			<!-- Le message d'erreur pour "texte" -->
 			@error("texte")
 			<div>{{ $message }}</div>
 			@enderror
 		</p>
+	</div>
         <p>
 			
 			<input type="hidden" name="user_id" value="{{Auth::user()->id}}"  id="user_id">
@@ -31,7 +34,9 @@
 			<div>{{ $message }}</div>
 			@enderror
 		</p>
-        
-        <x-primary-button>{{ __('Save') }}</x-primary-button>
+
+        <div class="p-2 flex justify-center">
+        <x-primary-button>{{ __('Sauvegarder') }}</x-primary-button>
+		</div>
       </form>
 </section>
