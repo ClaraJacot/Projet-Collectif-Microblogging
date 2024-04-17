b903b11e5f67d2c52a53628f5cdba7208e44d0be
mame <mbacke.mamediarra@gmail.com>
2024-04-17 16:55:10 +0200

    button make a post dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,13 @@
 <x-app-layout>
     
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <div class="flex justify-between items-center ">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight ">
             {{ __('Actualités') }}
         </h2>
+        
+        <button class="bg-gray-900 text-white font-bold py-2 px-4 rounded m-4 dark:bg-gray-200 dark:text-gray-800" onclick="location.href='{{ route('myProfile', ['id'=>Auth::user()->id]) }}'">Créer un post</button>
+        </div>
     </x-slot>
 
     <div class="py-12">
